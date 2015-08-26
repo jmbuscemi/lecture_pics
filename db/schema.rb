4308e44fc46a8923577ac599c0369a1fc669f529
John M Buscemi <jmbuscemi@gmail.com>
2015-08-26 15:01:39 -0400

    Update User model with first/last name

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826180724) do
+ActiveRecord::Schema.define(version: 20150826184240) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
