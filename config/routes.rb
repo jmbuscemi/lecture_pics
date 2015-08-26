6ec82f2d6aaeebce040d84c27d88a359d9cf9d1c
John M Buscemi <jmbuscemi@gmail.com>
2015-08-26 15:01:52 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   get 'sessions/login'
+  get 'login' => 'sessions#login'
+
+  post 'sessions/login'
+  post 'login' => 'sessions#login'
 
   get 'sessions/logout'
+  get 'logout' => 'sessions#logout'
 
   resources :users
 
   root 'users#show'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
