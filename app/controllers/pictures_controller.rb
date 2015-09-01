79841ee7fcd62636645488ab04165037b927bb8f
John M Buscemi <jmbuscemi@gmail.com>
2015-09-01 08:53:04 -0400

    Remove extraneous Picture views/actions/tests...basically undo most of scaffold

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,25 +1,10 @@
 class PicturesController < ApplicationController
-  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :set_picture, only: [:destroy]
 
-  # GET /pictures
-  def index
-    @pictures = Picture.all
-  end
-
-  # GET /pictures/1
-  def show
-  end
-
-  # GET /pictures/new
   def new
     @picture = Picture.new
   end
 
-  # GET /pictures/1/edit
-  def edit
-  end
-
-  # POST /pictures
   def create
     @picture = Picture.new(picture_params)
     @picture.user_id = session[:user_id]
@@ -30,30 +15,17 @@ class PicturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pictures/1
-  def update
-    if @picture.update(picture_params)
-      redirect_to @picture, notice: 'Picture was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /pictures/1
   def destroy
     @picture.destroy
     redirect_to pictures_url, notice: 'Picture was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_picture
       @picture = Picture.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def picture_params
-      # Only allow a trusted parameter "white list" through.
       params.require(:picture).permit(:id, :uploaded_file, :user_id)
     end
 end
--- test/controllers/pictures_controller_test.rb
+++ test/controllers/pictures_controller_test.rb
@@ -6,41 +6,18 @@ class PicturesControllerTest < ActionController::TestCase
     @user = users(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:pictures)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  #
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
   test "should create picture" do
     assert_difference('Picture.count') do
-      post :create, picture: {user_id: @user.id, uploaded_file_file_name: "example.jpg",
-          uploaded_file_content_type: "", uploaded_file_file_size: "",
-          uploaded_file_updated_at: "" }
+      post :create, picture: { user_id: @user.id, uploaded_file_file_name: "example.jpg" }
     end
 
     assert_redirected_to root_path
-  end
-
-  test "should show picture" do
-    get :show, id: @picture
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @picture
-    assert_response :success
-  end
-
-  test "should update picture" do
-    patch :update, id: @picture, picture: { uploaded_file_file_name: "example.jpg",
-        user_id: @user.id }
-    assert_redirected_to picture_path(assigns(:picture))
   end
 
   test "should destroy picture" do
