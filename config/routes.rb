9470754a1ff75f44bff63ba1653d781ec5a626c3
John M Buscemi <jmbuscemi@gmail.com>
2015-09-09 16:56:08 -0400

    Fix foundation javascript bug so menu links work

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   post 'sessions/login'
   post 'login' => 'sessions#login'
 
-  get 'sessions/logout'
   get 'logout' => 'sessions#logout'
 
   root 'users#show'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
