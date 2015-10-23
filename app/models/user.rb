ff5b542784edc2098a3a21dfe498adf7c5b6c35b
John M Buscemi <jmbuscemi@gmail.com>
2015-10-23 14:20:56 -0400

    Add password length requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
 
   def name
     "#{first_name.capitalize} #{last_name.capitalize}"
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -21,7 +21,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should get my_profile page" do
-    get :my_profile, id: @user
+    get :my_profile
     assert_response :success
   end
 
@@ -33,7 +33,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should update user" do
     patch :update, id: @user, user: { email: @user.email, first_name: @user.first_name,
         password: "password" }
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to my_profile_path
   end
 
   test "should destroy user" do
